8330a83bf36ef936cca979a967d416dafd8ea20e
Nick Tsai <neongreentiger8@gmail.com>
2025-11-26 16:10:10 -0800

    fixed big test

--- pneumatics.py
+++ pneumatics.py
@@ -1,23 +1,20 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
+def solenoid(power,ground,opened,delay):
+    GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(power,GPIO.OUT)
+    GPIO.setup(ground,GPIO.OUT)
+    GPIO.output(ground,False)
+    if opened == True:
+        GPIO.output(power,True)
+    elif opened == False:
+        GPIO.output(power,False)
+    time.sleep(delay)
+    print('pneumatics done')
 
-GPIO.setup(18,GPIO.OUT)
-GPIO.setup(23,GPIO.OUT)
+# solenoid(18,23,True)
+# solenoid(18,23,False)
 
-GPIO.output(23,False)
-
-for i in range(1,101):
-    GPIO.output(18,True)
-
-    time.sleep(1)
-
-    GPIO.output(18,False)
-
-    time.sleep(1)
-
-    print(i)
-
-GPIO.cleanup()
+# GPIO.cleanup()
