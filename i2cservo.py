d00db6d62cfc41c1ad702c578c92ba65a63515bb
Nick Tsai <neongreentiger8@gmail.com>
2025-11-28 09:09:26 -0800

    rowing v1

--- i2cservo.py
+++ i2cservo.py
@@ -1,10 +1,14 @@
 from adafruit_servokit import ServoKit
 import time
 
-def miuzei_servo(angle,delay):
-    kit = ServoKit(channels=16)
-    kit.servo[0].actuation_range = 270
-    kit.servo[0].set_pulse_width_range(500, 2500)
-    kit.servo[0].angle = angle
+def miuzei_servo(device,angle,delay):
+    miuzei = ServoKit(channels=16)
+    miuzei.servo[device].actuation_range = 270
+    miuzei.servo[device].set_pulse_width_range(500, 2900)
+    miuzei.servo[device].angle = angle
     time.sleep(delay)
     print('miuzei done')
+
+for i in range(0,10):
+    miuzei_servo(0,45,0.5)
+    miuzei_servo(0,110,0.5)
