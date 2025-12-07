4a1f7b41861d5cbcd928cae09c90bc125d78a671
Nick Tsai <neongreentiger8@gmail.com>
2025-12-06 22:38:01 -0800

    speaker and microservo

--- i2cservo.py
+++ i2cservo.py
@@ -9,6 +9,14 @@ def miuzei_servo(device,angle,delay):
     time.sleep(delay)
     print('miuzei done')
 
-# for i in range(0,10):
-#     miuzei_servo(0,45,0.5)
-#     miuzei_servo(0,110,0.5)
+def miuzei_micro(device,angle,delay):
+    miuzei = ServoKit(channels=16)
+    miuzei.servo[device].actuation_range = 180
+    miuzei.servo[device].set_pulse_width_range(400, 2900)
+    miuzei.servo[device].angle = angle
+    time.sleep(delay)
+    print('miuzei micro done')
+
+for i in range(0,10):
+    miuzei_micro(0,0,0.5)
+    miuzei_micro(0,180,0.5)
---
+++ speaker.py
@@ -0,0 +1,43 @@
+import serial
+import time
+
+seri = serial.Serial(port="/dev/serial0", baudrate=9600, timeout=1)
+
+def dfplayer_send(cmd, param1=0, param2=0):
+
+    cmd_line = [0x7E, 0xFF, 0x06, cmd, 0x00, param1, param2]
+    checksum = 0 - sum(cmd_line[1:]) 
+    checksum &= 0xFFFF
+
+    high = (checksum >> 8) & 0xFF
+    low = checksum & 0xFF
+
+    packet = bytes(cmd_line + [high, low, 0xEF])
+    seri.write(packet)
+    time.sleep(0.05)
+
+def set_volume(level):
+    dfplayer_send(0x06, 0x00, level)
+
+def play_track(num):
+    dfplayer_send(0x03, (num >> 8) & 0xFF, num & 0xFF)
+    print('file', num)
+
+def stop():
+    dfplayer_send(0x16)
+
+print("Init")
+time.sleep(2)
+
+set_volume(10)   
+play_track(1)     
+time.sleep(5)
+
+set_volume(20)
+play_track(2)     
+time.sleep(5)
+
+set_volume(30)
+play_track(3)   
+time.sleep(5)
+stop()
