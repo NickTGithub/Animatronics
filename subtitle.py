e3d9f35d4db960d101e5c3563afb987c6ed1c147
nicktsai <neongreentiger8@gmail.com>
2026-05-17 15:38:55 -0700

    ji

--- subtitle.py
+++ subtitle.py
@@ -2,17 +2,13 @@ import cv2
 import numpy as np
 import time
 
-screen = np.zeros((480, 680, 3), dtype=np.uint8)
+img = np.zeros((480, 1280, 3), dtype='uint8')
 
-cv2.rectangle(screen, (0, 0), (480, 480), (0, 255, 0), thickness=cv2.FILLED)
+cv2.putText(img, "Hello World", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 5)
 
-cv2.circle(screen, (240, 240), 10, (255, 0, 0), thickness=cv2.FILLED)
 
-# cv2.namedWindow("window", cv2.WINDOW_NORMAL)
-
-# cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-cv2.imshow("window", screen)
-
-time.sleep(100)
+cv2.namedWindow("window", cv2.WINDOW_NORMAL)
+cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+cv2.imshow("window", img)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
