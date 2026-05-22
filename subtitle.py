77cf6233e568f62712314e1e98c164f102c50b08
nicktsai <neongreentiger8@gmail.com>
2026-05-21 17:19:45 -0700

    asdf

--- subtitle.py
+++ subtitle.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 import time
 
-img = np.zeros((480, 1280, 3), dtype='uint8')
-
-cv2.putText(img, "Hello World", (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 3, (255, 255, 255), 5)
-
-
+capture = cv.VideoCapture(file)
 cv2.namedWindow("window", cv2.WINDOW_NORMAL)
 cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-cv2.imshow("window", img)
-cv2.waitKey(0)
+
+while True:
+    isTrue, frame = capture.read()
+    cv2.imshow("window", frame)
+    if cv2.waitKey(20) & 0xFF == ord('d'):
+        break
+
 cv2.destroyAllWindows()
