20aeabdf30db2871e91aee8cd49f641c73015422
nicktsai <neongreentiger8@gmail.com>
2026-05-21 18:18:33 -0700

    sd

--- subtitle.py
+++ subtitle.py
@@ -1,15 +1,39 @@
 import cv2
 import numpy as np
 import time
+#export DISPLAY=:0
 
-capture = cv.VideoCapture(file)
-cv2.namedWindow("window", cv2.WINDOW_NORMAL)
-cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+def playVid(file):
 
-while True:
-    isTrue, frame = capture.read()
-    cv2.imshow("window", frame)
-    if cv2.waitKey(20) & 0xFF == ord('d'):
-        break
+    cv2.destroyAllWindows()
 
+    if file < 10:
+        file = '0' + str(file)
+    else:
+        file = str(file)
+    file = 'subtitles/' + file + '.mp4'
+    capture = cv2.VideoCapture(file)
+    cv2.namedWindow("window", cv2.WINDOW_AUTOSIZE)
+    cv2.setWindowProperty("window", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    while True:
+        isTrue, frame = capture.read()
+        
+        if cv2.waitKey(20) & 0xFF == ord('d') or not isTrue:
+            break
+        cv2.imshow("window", frame)
+    cv2.destroyAllWindows()
+    
+def showBg():
+    
+    bg = np.zeros((1280, 480, 3), dtype=np.uint8)
+    cv2.namedWindow("back", cv2.WINDOW_AUTOSIZE)
+    cv2.setWindowProperty("back", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    bg[1280: , 480: ] = (0, 0, 0)
+    cv2.imshow("back", bg)
+
+#showBg()
+playVid(1)
+#showBg()
+time.sleep(2)
 cv2.destroyAllWindows()
