6288d4a5b7f7d435b4ecfb390620770c1ad1fa7b
NickT <147011013+NickTGithub@users.noreply.github.com>
2026-06-10 10:57:37 -0700

    Update i2cservo.py

--- i2cservo.py
+++ i2cservo.py
@@ -7,15 +7,17 @@ import busio
 
 def miuzei_servo(device,angle,delay):
     miuzei = ServoKit(channels=16)
-    miuzei.servo[device].actuation_range = 270
-    miuzei.servo[device].set_pulse_width_range(500, 2900)
-    miuzei.servo[device].angle = angle
+    for i in device:
+        miuzei.servo[device[i]].actuation_range = 270
+        miuzei.servo[device[i]].set_pulse_width_range(500, 2900)
+        miuzei.servo[device[i]].angle = angle[i]
     time.sleep(delay)
 
 
 def miuzei_micro(device,angle,delay):
     miuzei = ServoKit(channels=16)
-    miuzei.servo[device].actuation_range = 180
-    miuzei.servo[device].set_pulse_width_range(500, 2500)
-    miuzei.servo[device].angle = angle
+    for i in device:
+        miuzei.servo[device[i]].actuation_range = 180
+        miuzei.servo[device[i]].set_pulse_width_range(500, 2500)
+        miuzei.servo[device[i]].angle = angle[i]
     time.sleep(delay)
